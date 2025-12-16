dc6588eb5a44e898f13e15f0d9899c2ee9f1a661
Oleksij Romanenko <exxuslee@gmail.com>
2025-12-16 13:23:25 +0200

    feat: add player selection handling and update player actions in GameViewModel

--- app/src/main/kotlin/com/exxuslee/munchkinsimplecounter/features/game/GameScreen.kt
+++ app/src/main/kotlin/com/exxuslee/munchkinsimplecounter/features/game/GameScreen.kt
@@ -1,8 +1,12 @@
 package com.exxuslee.munchkinsimplecounter.features.game
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import com.exxuslee.munchkinsimplecounter.R
 import com.exxuslee.munchkinsimplecounter.features.game.models.Action
 import org.koin.androidx.compose.koinViewModel
 
@@ -20,7 +24,14 @@ fun GameScreen(
 
     when (viewAction) {
 
-        Action.ShowSelectPlayerMessage -> {}
+        Action.ShowSelectPlayerMessage -> {
+            Toast.makeText(
+                LocalContext.current,
+                stringResource(R.string.select_player_toast_message),
+                Toast.LENGTH_SHORT
+            ).show()
+            viewModel.clearAction()
+        }
         null -> {}
     }
 }
