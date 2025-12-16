39cc1e5282fd559cf5b3f9f4744c555cce863af8
Oleksij Romanenko <exxuslee@gmail.com>
2025-12-16 15:05:26 +0200

    feat: update game label to 'Munchkin counter' in Routes and SettingsView

--- app/src/main/kotlin/com/exxuslee/munchkinsimplecounter/features/game/GameView.kt
+++ app/src/main/kotlin/com/exxuslee/munchkinsimplecounter/features/game/GameView.kt
@@ -6,13 +6,20 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.exxuslee.munchkinsimplecounter.R
 import com.exxuslee.munchkinsimplecounter.features.game.models.Event
 import com.exxuslee.munchkinsimplecounter.features.game.models.ViewState
+import com.exxuslee.munchkinsimplecounter.ui.common.ListEmptyView
 
 @Composable
 fun GameView(viewState: ViewState, eventHandler: (Event) -> Unit) {
     Column {
+        if (viewState.players.isEmpty()) ListEmptyView(
+            text = stringResource(R.string.game_empty_players_list),
+            icon = R.drawable.outline_empty_dashboard_24
+        )
         PlayerCard(null)
         LazyColumn(
             modifier = Modifier.weight(1f),
