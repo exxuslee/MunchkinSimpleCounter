d1dc5d2ce96b70fea8a457b0c7ae235a09a109a6
Oleksij Romanenko <exxuslee@gmail.com>
2025-12-27 01:00:06 +0200

    feat: update donation URI generation to support token transfers and improve compatibility

--- app/src/main/kotlin/com/exxuslee/munchkinsimplecounter/features/settings/donate/DonateScreen.kt
+++ app/src/main/kotlin/com/exxuslee/munchkinsimplecounter/features/settings/donate/DonateScreen.kt
@@ -2,10 +2,12 @@ package com.exxuslee.munchkinsimplecounter.features.settings.donate
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import com.exxuslee.munchkinsimplecounter.features.settings.donate.models.Action
 import org.koin.androidx.compose.koinViewModel
 
@@ -27,6 +29,11 @@ fun DonateScreen(
             viewModel.clearAction()
             val uri = (viewAction as Action.Donate).uri
             val intent = Intent(Intent.ACTION_VIEW, uri)
+//            Toast.makeText(
+//                LocalContext.current,
+//                uri.toString(),
+//                Toast.LENGTH_SHORT
+//            ).show()
 
             if (intent.resolveActivity(LocalContext.current.packageManager) != null) {
                 LocalContext.current.startActivity(Intent.createChooser(intent, "Choose wallet"))
